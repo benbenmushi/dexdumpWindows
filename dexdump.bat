e73c3d4ef2442538d41853addd436f98e76ecd43
Hallion Benjamin <benbenmushi@gmail.com>
2018-04-20 18:15:09 +0200

    Udpate dexdump.bat to take .apk instead of .dex

--- dexdump.bat
+++ dexdump.bat
@@ -1,3 +1,11 @@
+set "tempFolder=%tmp%\bat~%RANDOM%"
+
 for %%x in (%*) do (
-  %~dp0\dexdump.exe -l plain %%x > %%xdump.txt
+ echo "%%~nx"
+ "C:\Program Files\7-Zip\7z.exe" e "%%x" "-o%tempFolder%" *.dex -r -aoa
+ for %%i in (%tempFolder%\*.dex) do (
+ if not exist "%%~dpnx_dexdump" mkdir "%%~dpnx_dexdump"
+  %~dp0\dexdump.exe -l plain "%tempFolder%\%%~nxi" > "%%~dpnx_dexdump\%%~nxidump.txt"
+)
+ %SystemRoot%\explorer.exe "%%~dpnx_dexdump"
 )
